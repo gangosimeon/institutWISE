2066f3bd66b5b1a064c5f17bfaa3f4282e17d16b
Simeon <gangosimeon@gmail.com>
2024-11-28 11:26:57 +0000

    Initialisation du projet angular Instutit WISE

---
+++ frontend/angular/laBonneAffaire/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component , OnInit} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+  title = 'laBonneAffaire';
+  constructor(){}
+  ngOnInit(): void {
+    console.log("Ici OnInit");
+  }
+}
