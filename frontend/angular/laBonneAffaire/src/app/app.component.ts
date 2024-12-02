93d7f7f809f96469a5cae64e9503cbfc8e52e885
Simeon <gangosimeon@gmail.com>
2024-12-02 17:37:43 +0000

    Utilisation du databinding

--- frontend/angular/laBonneAffaire/src/app/app.component.ts
+++ frontend/angular/laBonneAffaire/src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'laBonneAffaire';
+  prix1:number=24;
+  prix2:number=245;
+  prix3:number=47;
+  message:string="";
+  
+  onAffiche(event:string){
+    return this.message="Vous avez liker l'article : "+event;
+  }
 }
--- frontend/angular/laBonneAffaire/src/app/article/article.component.ts
+++ frontend/angular/laBonneAffaire/src/app/article/article.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
   styleUrls: ['./article.component.css']
 })
-export class ArticleComponent {
+export class ArticleComponent implements OnInit {
+  // titreArticle:string="Titre de l'article";
+  // prixArticle:number=12;
+  urlImg:string="https://via.placeholder.com/400x250"
+  textAlternative:string= "texte alternative de l'image."
+  nbreTotalLike:number=0;
+  comment:string="Ceci est un commentaire "
+  // Données à transmettre du parent vers l'enfant
+  @Input() titreArticle:string;
+  @Input() prixArticle:number;
 
+  //Données à transmettre de l'enfant vers le parents
+  @Output() info = new EventEmitter<string>();
+
+  ngOnInit(): void {
+  
+
+  }
+
+  onLike(){
+    this.nbreTotalLike++;
+    this.info.emit(this.titreArticle);
+  }
 }
