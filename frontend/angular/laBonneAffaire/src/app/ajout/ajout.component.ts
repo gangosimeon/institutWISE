2644c5a31584ad6f5ba8967e958b3dced3109914
Simeon <gangosimeon@gmail.com>
2024-12-05 13:03:17 +0000

    Gestion des formulaires

--- frontend/angular/laBonneAffaire/src/app/ajout/ajout.component.ts
+++ frontend/angular/laBonneAffaire/src/app/ajout/ajout.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-ajout',
   templateUrl: './ajout.component.html',
   styleUrls: ['./ajout.component.css']
 })
-export class AjoutComponent {
+export class AjoutComponent implements OnInit {
 
+  titleToAdd="";
+  priceToAdd="";
+  ngOnInit(): void {
+    
+  }
+  onSubmit(form:NgForm){
+    const newTitle = this.titleToAdd;
+    const newPrice = this.priceToAdd;
+    console.log(`newTitle: ${newTitle}  et newPrice : ${newPrice}`);
+  }
 }
