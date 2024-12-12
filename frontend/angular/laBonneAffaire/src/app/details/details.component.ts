25483398d4c3365859ec773b8413dbf1455954fb
Gango Simeon <gangosimeon@gmail.com>
2024-12-12 15:32:44 +0000

    :

--- frontend/angular/laBonneAffaire/src/app/details/details.component.ts
+++ frontend/angular/laBonneAffaire/src/app/details/details.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  constructor( 
+    private activatesdRoute: ActivatedRoute,
+    private dataService:DataService
+  ) { }
+  ngOnInit(): void {
+    const id = this.activatesdRoute.snapshot.params['id'];
+    console.log(id);
+    this.getArticleById(id);
+  }
 
+
+  getArticleById(id:number){
+    this.dataService.getAllArticlesServe().subscribe(articles=>{
+      articles.find(articles=>{
+        return articles.id===id;
+      })
+    });
+  }
 }
