5248c68b5e38925feee9c307dcd6c605a78b581f
Simeon <gangosimeon@gmail.com>
2024-12-16 17:49:32 +0000

    implementation de details article

--- frontend/angular/laBonneAffaire/src/app/data.service.ts
+++ frontend/angular/laBonneAffaire/src/app/data.service.ts
@@ -22,36 +22,36 @@ export class DataService {
   // }
 
 
-  listeArticles=[
-    {
-      id:1,
-      titreArticle:"Vélo",
-      prixArticle:50,
-      urlImg:"/assets/velo.jpg",
-      description:"Le vélo, écologique et sain, procure une liberté de détente sur deux roues.",
-      textAlternative:"Ceci est un super velo",
-      nbreTotalLike:0,
-      dispo :false,
-    },
-    {
-      id:2,
-      titreArticle:"Voiture",
-      prixArticle:650,
-      urlImg:"/assets/voiture.jpg",
-      description:"La voiture, confortable et pratique, assure un voyage en toute sécurité sur quatre roues.",
-      textAlternative:"Ceci est une voiture tout terrain",
-      nbreTotalLike:0,
-      dispo :true,
-    },
-    {
-      id:3,
-      titreArticle:"Moto",
-      prixArticle:450,
-      urlImg:"/assets/moto.jpg",
-      description:"La moto, rapide et agile, offre une liberté d'aventure sur deux roues.",
-      textAlternative:"Ceci est une moto",
-      nbreTotalLike:0,
-      dispo :true,
-    }
-  ]
+  // listeArticles=[
+  //   {
+  //     id:1,
+  //     titreArticle:"Vélo",
+  //     prixArticle:50,
+  //     urlImg:"/assets/velo.jpg",
+  //     description:"Le vélo, écologique et sain, procure une liberté de détente sur deux roues.",
+  //     textAlternative:"Ceci est un super velo",
+  //     nbreTotalLike:0,
+  //     dispo :false,
+  //   },
+  //   {
+  //     id:2,
+  //     titreArticle:"Voiture",
+  //     prixArticle:650,
+  //     urlImg:"/assets/voiture.jpg",
+  //     description:"La voiture, confortable et pratique, assure un voyage en toute sécurité sur quatre roues.",
+  //     textAlternative:"Ceci est une voiture tout terrain",
+  //     nbreTotalLike:0,
+  //     dispo :true,
+  //   },
+  //   {
+  //     id:3,
+  //     titreArticle:"Moto",
+  //     prixArticle:450,
+  //     urlImg:"/assets/moto.jpg",
+  //     description:"La moto, rapide et agile, offre une liberté d'aventure sur deux roues.",
+  //     textAlternative:"Ceci est une moto",
+  //     nbreTotalLike:0,
+  //     dispo :true,
+  //   }
+  // ]
 }
