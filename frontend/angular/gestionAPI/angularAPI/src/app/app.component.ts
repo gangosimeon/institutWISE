97842626ec99a05f0912a4c5c49974e21aed8874
Simeon <gangosimeon@gmail.com>
2024-12-17 13:18:33 +0000

    Second commit today

--- frontend/angular/gestionAPI/angularAPI/src/app/app.component.ts
+++ frontend/angular/gestionAPI/angularAPI/src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angularAPI';
+  contacts:any
+  constructor(private httpClient:HttpClient){}
+  ngOnInit(): void {
+    this.getContacts()
+  }
+
+  getContacts(){
+    return this.httpClient.get('http://localhost:3000/contact').subscribe((contacts)=>{
+      this.contacts=contacts
+    });
+    
+  }
 }
